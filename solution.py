8fbb40e390d9ca9b79e61f33996fcc6ca389cd13
ghostdivisio <112753558+ghostdivisio@users.noreply.github.com>
2023-04-10 17:09:59 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,4 +5,4 @@ from scipy import stats
 chat_id = 1326113898 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    return stats.cramervonmises_2samp(x, y).pvalue < 0.05
+    return stats.anderson_ksamp(x, y).pvalue < 0.05
